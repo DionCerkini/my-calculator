8be5c0608484abc222274209ac537f3f0c839137
MezinK <mezinkocahal@hotmail.com>
2023-10-05 14:24:03 +0200

    delete unnecessary classes

--- src/main/java/ch/dion/calculator/services/TodoService.java
+++
@@ -1,6 +0,0 @@
-package ch.dion.calculator.services;
-import java.util.List;
-public interface TodoService {
-    // Creating a simple method retrieveTodos()
-    public List<String> retrieveTodos(String user);
-}
